af2219d01dde13dad934f87d08a6717aa0daa69f
elhusseinn <elhusseinabdalaziz1@gmail.com>
2023-04-27 01:46:13 +0200

    finished page handeling

--- src/app/page-render/page-render.component.ts
+++ src/app/page-render/page-render.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit ,Input} from '@angular/core';
 import { PageService } from '../shared-services/page/page.service';
+import { CssService } from '../shared-services/css.service';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
@@ -11,23 +12,26 @@ export class PageRenderComponent implements OnInit {
   @Input() id:any;
   page:any;
   modules:any;
-  title:any;
-  subtitle:any;
+  pageTitle:any;
   headerImage:any;
   coverImage:any;
   sanitizedContent!: SafeHtml;
-  constructor(private pageServ: PageService,private sanitizer:DomSanitizer) { }
+  constructor(private pageServ: PageService,private sanitizer:DomSanitizer, private cssServ : CssService) { }
 
   ngOnInit(): void {
     this.pageServ.getPages().subscribe(data =>{
     this.page=data 
-    this.title = this.page.title
-    this.subtitle = this.page.subtitle
     this.headerImage = this.page.header_image
     this.coverImage = this.page.cover_image
     this.modules=this.page.modules
+
+    this.modules.sort((a:any, b:any) => a.priority - b.priority);  // sort modules by priority
     this.modules.forEach((module:any)=>{  // goes through the content and make it safe to render
       module.content = this.sanitizer.bypassSecurityTrustHtml(module.content)
+      this.cssServ.getCss(module.class_id).subscribe(cssdata=>{
+        module.css_class = cssdata;
+        module.class = module.css_class.css;
+      });
     });
 
     })
---
+++ src/app/shared-services/css.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CssService {
+  private configUrl="http://localhost:3300/CssClasses"
+  constructor(private http : HttpClient) {
+
+   }
+
+   getCss(id:number){
+    const url = `${this.configUrl}/${id}`
+    return this.http.get(url)
+   }
+}
